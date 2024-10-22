4f05b0b9664134ea735096c1a42521b62ea243ae
Arif Khan <khanarif2738@gmail.com>
2024-10-22 16:00:07 +0530

    Run gsap small animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  // TODO: Implement the gsap.to() method
+  useGSAP(() => {
+    gsap.to('#blue-box', {
+      x: 250,
+      repeat: -1,
+      yoyo: true
+    })
+  }, []);  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id='blue-box' className='h-[160px] w-[160px] bg-blue-500 mx-auto rounded-lg'>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
